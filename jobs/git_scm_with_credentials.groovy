ba3446670e363e99e0527230ea796231ed4f845f
Manoj Badam <manoj.gitam@gmail.com>
2019-08-04 12:18:57 -0700

    fixing incorrect github url project

--- jobs/git_scm_with_credentials.groovy
+++ jobs/git_scm_with_credentials.groovy
@@ -2,7 +2,8 @@ job("Simple Git SCM with Credentials") {
   scm {
     git {
       remote {
-        github("https://github.com/JFrog/project-examples.git")
+        github("https://github.com/JFrog/project-examples")
+        url("https://github.com/JFrog/project-examples.git")
         credentials("user-name-password-credentials")
       }
     }
