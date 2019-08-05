fc7cb728b346dfaf465bb7ded7618e3647d0bd3a
Manoj Badam <manoj_badam@intuit.com>
2019-08-04 19:23:08 -0700

    fix class properties

--- src/com/badam/jenkins/Utils.groovy
+++ src/com/badam/jenkins/Utils.groovy
@@ -1,6 +1,6 @@
 package com.badam.jenkins
 
-class Utils {
+class Utils implements Serializable {
     def static fetchPublisheres() {
         return {
                 mailer('me@example.com', true, true)
